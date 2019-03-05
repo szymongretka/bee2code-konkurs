3902bc18f15b4cfc65f0fb259247ca08cebe8ee1
szymongretka <szymon.gretka@gmail.com>
2019-03-05 11:31:23 +0100

    First commit

---
+++ src/main/java/pl/szymongretka/bee2code/Bee2codeApplication.java
@@ -0,0 +1,13 @@
+package pl.szymongretka.bee2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bee2codeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bee2codeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/szymongretka/bee2code/Bee2codeApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.szymongretka.bee2code;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bee2codeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
